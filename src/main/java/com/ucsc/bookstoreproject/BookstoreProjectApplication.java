b9d565fabb1027172d847d6064fc1fd67e028acd
lasithpalihawadana <lasithpalihawadana@gmail.com>
2025-05-01 09:13:40 +0530

    Fix issue

---
+++ src/main/java/com/ucsc/bookstoreproject/BookstoreProjectApplication.java
@@ -0,0 +1,14 @@
+package com.ucsc.bookstoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreProjectApplication.class, args);
+    }
+
+
+}
